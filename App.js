58c4e1693c21eaf7b054f965dbd55d18b91bb9c5
Daniel Nam <dannam83@gmail.com>
2018-11-26 09:55:04 -0500

    fix TabNavigator

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen }
+    });
+    const AppContainer = createAppContainer(MainNavigator);
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
