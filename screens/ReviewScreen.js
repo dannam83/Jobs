a69aa2ce2ee118a08a63e3c70a08e8ac4cb5ec68
Daniel Nam <dannam83@gmail.com>
2018-11-26 22:27:25 -0500

    show right side of header

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs',
+    headerRight: <Text>hi</Text>
+  }
+
   render() {
     return (
       <View>
