fc84be43af4565b9c208b7fe7ca36c72041c47cc
Daniel Nam <dannam83@gmail.com>
2018-11-26 22:09:05 -0500

    add map and deck navigation

--- App.js
+++ App.js
@@ -4,12 +4,20 @@ import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = createBottomTabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: createBottomTabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen }
+        })
+      }
     });
     const AppContainer = createAppContainer(MainNavigator);
 
