769cbef0a00338670532499bb22c38aa48b32336
Daniel Nam <dannam83@gmail.com>
2018-11-27 07:59:59 -0500

    add last slide button on welcome

--- components/Slides.js
+++ components/Slides.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(idx) {
+    if (idx === this.props.data.length - 1) {
+      return (
+        <Button
+          title='Onwards!'
+          raised
+          buttonStyle={styles.slideButton}
+          onPress={this.props.onComplete}
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map((slide) => {
+    return this.props.data.map((slide, idx) => {
       return (
         <View
           key={slide.text}
           style={[styles.slide, { backgroundColor: slide.color }]}
         >
           <Text style={styles.slideText}>{slide.text}</Text>
+          {this.renderLastSlide(idx)}
         </View>
       );
     });
@@ -39,7 +54,12 @@ const styles = {
   },
   slideText: {
     fontSize: 30,
-    color: 'white'
+    color: 'white',
+    textAlign: 'center'
+  },
+  slideButton: {
+    backgroundColor: '#0288D1',
+    marginTop: 15
   }
 }
 
