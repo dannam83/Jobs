29acb2620f0c2dcba526f8557b4c9a761bcf5a2b
Daniel Nam <dannam83@gmail.com>
2018-12-01 12:21:47 -0500

    setup card

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
+import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    return (
+      <Card title={job.jobTitle}>
+
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
 }
 
-export default DeckScreen;
+function mapStateToProps({ jobs }) {
+  return { jobs: jobs.results }
+}
+
+export default connect(mapStateToProps)(DeckScreen);
