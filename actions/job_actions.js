75e652b5fca0eac958466264f64a2ad14220c788
Daniel Nam <dannam83@gmail.com>
2018-12-01 12:07:14 -0500

    setup redirect after fetch jobs

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -24,13 +24,14 @@ const buildJobsUrl = (zip) => {
   return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = (region) => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try {
     let zip = await Location.reverseGeocodeAsync(region);
     const url = buildJobsUrl(zip);
     // let { data } = await axios.get(url);
     // console.log(JOB_DATA);
-    dispatch({ type: FETCH_JOBS, payload: JOB_DATA })
+    dispatch({ type: FETCH_JOBS, payload: JOB_DATA });
+    callback();
   } catch(e) {
     console.log(e);
   }
