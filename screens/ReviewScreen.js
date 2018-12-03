ee58e8d78938cf128394d9f9350f3afd817ce580
Daniel Nam <dannam83@gmail.com>
2018-12-03 08:20:10 -0500

    add map to review job card

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Platform, ScrollView, Linking } from 'react-native';
 import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -24,10 +25,22 @@ class ReviewScreen extends Component {
   renderLikedJobs() {
     return this.props.likedJobs.map(job => {
       const { company, formattedRelativeTime, url } = job;
+      const initialRegion = {
+        longitude,
+        latitude,
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.02
+      };
 
       return (
         <Card>
           <View style={{ height: 200 }}>
+            <MapView
+              cacheEnabled={Platform.OS === 'android'}
+              scrollEnabled={false}
+              initialRegion={initialRegion}
+            >
+            </MapView>
             <View style={styles.detailWrapper}>
               <Text style={styles.italics}>{company}</Text>
               <Text style={styles.italics}>{formattedRelativeTime}</Text>
